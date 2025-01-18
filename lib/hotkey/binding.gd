f814151ea6e4ab7122b89732360526a18e2c7058
erodozer <ero@erodozer.moe>
2025-01-18 15:04:16 -0500

    handle recording hotkey input

--- lib/hotkey/binding.gd
+++ lib/hotkey/binding.gd
@@ -10,9 +10,16 @@ var action: Action
 var button_1: String
 var button_2: String
 var button_3: String
-var _listen_to_input : bool :
+var listen_to_input : bool :
 	get():
 		return button_1 or button_2 or button_3
+var input_as_list: Array :
+	get():
+		var l = []
+		for b in [button_1, button_2, button_3]:
+			if b != "":
+				l.append(b)
+		return l
 
 var screen_button: int
 var screen_button_color: Color
@@ -26,17 +33,22 @@ var deactivate_on_keyup = false
 signal activate
 signal deactivate
 
-func _is_pressed(button) -> bool:
-	if button.starts_with("KEY_"):
-		return Input.is_key_pressed(button)
-	if button.starts_with("MOUSE_"):
-		return Input.is_mouse_button_pressed(button)
-	if button.starts_with("JOY_"):
-		return Input.is_joy_button_pressed(0, button)
+func _is_pressed(button: String) -> bool:
+	var keycode = OS.find_keycode_from_string(button)
+	if keycode != KEY_NONE:
+		return Input.is_key_pressed(OS.find_keycode_from_string(button))
+	if button.begins_with("MOUSE_"):
+		if button == "MOUSE_BUTTON_LEFT":
+			return Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)
+		elif button == "MOUSE_BUTTON_RIGHT":
+			return Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)
+	if button.begins_with("JOY_"):
+		return false
+		# return Input.is_joy_button_pressed(0, button)
 	return true
 
 func _process(delta: float) -> void:
-	if not _listen_to_input:
+	if not self.listen_to_input:
 		return
 		
 	var b1 = _is_pressed(button_1)
--- studio/ui/hotkey_panel/hotkey_panel.gd
+++ studio/ui/hotkey_panel/hotkey_panel.gd
@@ -5,19 +5,52 @@ const HotkeyBinding = preload("res://lib/hotkey/binding.gd")
 
 @onready var list = get_node("%List")
 
+func _ready() -> void:
+	set_process_input(true)
+
 func _on_model_manager_model_changed(model: VtModel) -> void:
 	for c in list.get_children():
 		c.queue_free()
 		
 	for binding in model.hotkeys:
 		var panel = preload("./hotkey_setting.tscn").instantiate()
+		panel.set_meta("hotkey", binding)
 		panel.get_node("%HotkeyName").text = binding.name
 		match binding.action:
 			HotkeyBinding.Action.TOGGLE_EXPRESSION:
 				panel.get_node("%ActionSelect").text = "Toggle Expression (%s)" % binding.file.get_file()
-		panel.get_node("%Button1").text = binding.button_1
-		panel.get_node("%Button2").text = binding.button_2
-		panel.get_node("%Button3").text = binding.button_3
+		if binding.listen_to_input:
+			panel.get_node("%InputBinding").text = " + ".join(binding.input_as_list)
+		else:
+			panel.get_node("%InputBinding").text = ""
 		panel.get_node("%FadeDuration").value = binding.duration
+		panel.get_node("%Rec").pressed.connect(record_input.bind(panel))
 		
 		list.add_child(panel)
+
+func record_input(setting):
+	var hotkey = setting.get_meta("hotkey")
+	
+	%Modal.visible = true
+	%InputRecPopup.show()
+	%InputRecPopup.grab_focus()
+	
+	var pressed: Array = await %InputRecPopup.input_recorded
+	
+	%InputRecPopup.hide()
+	%Modal.visible = false
+	
+	if len(pressed) > 0:
+		hotkey.button_1 = OS.get_keycode_string(pressed[0])
+	else:
+		hotkey.button_1 = ""
+	if len(pressed) > 1:
+		hotkey.button_2 = OS.get_keycode_string(pressed[1])
+	else:
+		hotkey.button_2 = ""
+	if len(pressed) > 2:
+		hotkey.button_3 = OS.get_keycode_string(pressed[2])
+	else:
+		hotkey.button_3 = ""
+	
+	setting.get_node("%InputBinding").text = " + ".join(hotkey.input_as_list)
