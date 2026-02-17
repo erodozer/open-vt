6d194d996c67a8c5ec3e71ce9cdce1fa02fbe683
erodozer <ero@erodozer.moe>
2026-02-17 14:55:17 -0500

    improve developer build script

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -8,7 +8,13 @@ pip install pipx scons
 # build gd_cubism
 build_gdcubism () {
 	WORKDIR=$PROJ_ROOT/thirdparty/gd_cubism
+	ADDONDIR=$PROJ_ROOT/addons/gd_cubism
+	NATIVELIB=$(find $WORKDIR/thirdparty -type f -iwholename "*/linux/x86_64/libLive2DCubismCore.so" | head -n 1)
 
+	echo "CUBISM NATIVE LIBRARY:" $NATIVELIB
+
+	mkdir $ADDONDIR
+	
 	cd $WORKDIR
 	
 	## Cubism SDK must be acquired and prepared ahead of time
@@ -16,43 +22,62 @@ build_gdcubism () {
 	##
 	## https://www.live2d.com/en/sdk/download/native/
 	##
-	## OpenVT is currently developed against Native version 5 R3
+	## OpenVT is currently developed against Native version 5 R4.1
 	scons platform=linux arch=x86_64 target=template_debug debug_symbols=yes GDCUBISM_DYLIB=1
-	cp -r $WORKDIR/demo/addons/gd_cubism $PROJ_ROOT/addons
-	rm -r $PROJ_ROOT/addons/gd_cubism/example
-	rm -r $PROJ_ROOT/addons/gd_cubism/cs
-	cp LICENSE.en.adoc $PROJ_ROOT/addons/gd_cubism/LICENSE.en.adoc
-	cp README.en.adoc $PROJ_ROOT/addons/gd_cubism/README.en.adoc
+	cp -r $WORKDIR/demo/addons/gd_cubism/bin $ADDONDIR
+	cp -r $WORKDIR/demo/addons/gd_cubism/res $ADDONDIR
+	cp $WORKDIR/demo/addons/gd_cubism/gd_cubism.gdextension $ADDONDIR/gd_cubism.gdextension
 	
-	echo "*" > $PROJ_ROOT/addons/gd_cubism/.gitignore
+	cp $NATIVELIB $ADDONDIR/bin/linux/x86_64/libLive2DCubismCore.so
+
+	cp LICENSE.en.adoc $ADDONDIR/LICENSE.en.adoc
+	cp README.en.adoc $ADDONDIR/README.en.adoc
 	
+	echo "*" > $ADDONDIR/.gitignore
+	
+	# log completion
+	find $ADDONDIR -type f
+
 	cd $PROJ_ROOT
 }
 
 build_virtualcamera () {
 	WORKDIR=$PROJ_ROOT/thirdparty/gd-virtualcamera
+	ADDONDIR=$PROJ_ROOT/addons/gd-virtualcamera
 
+	mkdir $ADDONDIR
+	
 	cd $WORKDIR
 	
 	scons platform=linux arch=x86_64 target=template_debug debug_symbols=yes
-	scons platform=linux arch=x86_64 target=template_release
-	cp -r $WORKDIR/demo/bin $PROJ_ROOT/addons/gd-virtualcamera
-	cp LICENSE.md $PROJ_ROOT/addons/gd-virtualcamera/LICENSE.md
-	cp README.md $PROJ_ROOT/addons/gd-virtualcamera/README.md
-	echo "*" > $PROJ_ROOT/addons/gd-virtualcamera/.gitignore
+	cp -r $WORKDIR/demo/bin/* $ADDONDIR
+	cp LICENSE.md $ADDONDIR/LICENSE.md
+	cp README.md $ADDONDIR/README.md
+	echo "*" > $ADDONDIR/.gitignore
 	
+	# log completion
+	find $ADDONDIR -type f
+
 	cd $PROJ_ROOT
 }
 
 build_keylogger() {
 	WORKDIR=$PROJ_ROOT/thirdparty/godot-keylogger
+	ADDONDIR=$PROJ_ROOT/addons/keylogger
+
+	mkdir $ADDONDIR
 
 	cd $WORKDIR
 
 	$WORKDIR/build.sh
 
-	cp -r $WORKDIR/godot/addons/keylogger $PROJ_ROOT/addons
-	echo "*" > $PROJ_ROOT/addons/keylogger/.gitignore
+	cp -r $WORKDIR/godot/addons/keylogger/* $ADDONDIR
+	echo "*" > $ADDONDIR/.gitignore
+
+	# log completion
+	find $ADDONDIR -type f
+
+	cd $PROJ_ROOT
 }
 
 # fetch submodules if they haven't been already
