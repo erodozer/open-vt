cdbeea065b712bb9d227bbb194e0f7da6b4aa00f
erodozer <ero@erodozer.moe>
2026-04-03 12:26:01 -0400

    support VTS controller parameters
    - touchpad is currently unsupported
    - gyro parameters are unavailable until support is available through Godot 4.7

--- lib/tracking/gamepad/gamepad_tracker.gd
+++ lib/tracking/gamepad/gamepad_tracker.gd
@@ -1,10 +1,13 @@
-## Gamepad Tracking implements parameter compatibility with Nyarupad,
-## the defacto tracking extension for VTS.  This implementation is
-## targeted for the sake of maintaining portability of models from VTS
+## Gamepad Tracking implements parameter compatibility with VTS and Nyarupad,
+## This implementation is targeted for the sake of maintaining portability of models from VTS
 ##
 ## Nyarupad prefixes its custom parameters with NP_
+##   Nyarupad also had support for mapping DPAD to Analog inputs
+## Features from Nyarupad are also extended to VTS Gamepad Inputs
 
 extends "../tracker.gd"
+
+const DEADZONE = 0.1
 
 const DPAD = [
 	"NP_DPadDown",
@@ -50,132 +53,133 @@ static func is_dpad_pressed(inputs):
 
 @export var keyboard_mapping: Array = [
 	{
-		"input": "NP_DPadUp",
+		"input": ["NP_DPadUp"],
 		"name": "D-Up",
 		"key": KEY_W
 	},
 	{
-		"input": "NP_DPadDown",
+		"input": ["NP_DPadDown"],
 		"name": "D-Down",
 		"key": KEY_S
 	},
 	{
-		"input": "NP_DPadLeft",
+		"input": ["NP_DPadLeft"],
 		"name": "D-Left",
 		"key": KEY_A
 	},
 	{
-		"input": "NP_DPadRight",
+		"input": ["NP_DPadRight"],
 		"name": "D-Right",
 		"key": KEY_D
 	},
 	{
-		"input": "NP_ButtonA",
+		"input": ["NP_ButtonA", "ControllerCross"],
 		"name": "Cross/A",
 		"key": KEY_J
 	},
 	{
-		"input": "NP_ButtonB",
+		"input": ["NP_ButtonB", "ControllerCircle"],
 		"name": "Circle/B",
 		"key": KEY_K
 	},
 	{
-		"input": "NP_ButtonX",
+		"input": ["NP_ButtonX", "ControllerSquare"],
 		"name": "Square/X",
 		"key": KEY_U
 	},
 	{
-		"input": "NP_ButtonY",
+		"input": ["NP_ButtonY", "ControllerTriangle"],
 		"name": "Triangle/Y",
 		"key": KEY_I
 	},
 	{
-		"input": "NP_L1",
+		"input": ["NP_L1", "ControllerShoulderLeft"],
 		"name": "L1/LB",
 		"key": KEY_O
 	},
 	{
-		"input": "NP_L2",
+		"input": ["NP_L2", "ControllerTriggerLeft"],
 		"name": "L2/LT",
 		"key": KEY_P
 	},
 	{
-		"input": "NP_R1",
+		"input": ["NP_R1", "ControllerShoulderRight"],
 		"name": "R1/RB",
 		"key": KEY_L
 	},
 	{
-		"input": "NP_R2",
+		"input": ["NP_R2", "ControllerTriggerRight"],
 		"name": "R2/RT",
 		"key": KEY_SEMICOLON
 	},
 	{
-		"input": "NP_ButtonLS",
+		"input": ["NP_ButtonLS", "ControllerStickPressLeft"],
 		"name": "L3/LS",
 		"key": KEY_Y
 	},
 	{
-		"input": "NP_ButtonRS",
+		"input": ["NP_ButtonRS", "ControllerStickPressRight"],
 		"name": "R3/RS",
 		"key": KEY_H
 	},
 	{
-		"input": "NP_StartDown",
+		"input": ["NP_StartDown", "ControllerOptionRight"],
 		"name": "Start/Menu",
 		"key": KEY_ENTER
 	},
 	{
-		"input": "NP_SelectDown",
+		"input": ["NP_SelectDown", "ControllerOptionLeft"],
 		"name": "Opt/Back",
 		"key": KEY_TAB
 	},
 	{
-		"input": "NP_LStickX",
+		"input": ["NP_LStickX", "ControllerStickLeftX"],
 		"name": "LS X-",
 		"key": KEY_A,
 		"range": Vector2(0, -1.0)
 	},
 	{
-		"input": "NP_LStickX",
+		"input": ["NP_LStickX", "ControllerStickLeftX"],
 		"name": "LS X+",
 		"key": KEY_D
 	},
 	{
-		"input": "NP_LStickY",
+		"input": ["NP_LStickY", "ControllerStickLeftY"],
 		"name": "LS Y-",
 		"key": KEY_S,
 		"range": Vector2(0, -1.0)
 	},
 	{
-		"input": "NP_LStickY",
+		"input": ["NP_LStickY", "ControllerStickLeftY"],
 		"name": "LS Y+",
 		"key": KEY_W
 	},
 	{
-		"input": "NP_RStickX",
+		"input": ["NP_RStickX", "ControllerStickRightX"],
 		"name": "RS X-",
 		"key": KEY_LEFT,
 		"range": Vector2(0, -1.0)
 	},
 	{
-		"input": "NP_RStickX",
+		"input": ["NP_RStickX", "ControllerStickRightX"],
 		"name": "RS X+",
 		"key": KEY_RIGHT
 	},
 	{
-		"input": "NP_RStickY",
+		"input": ["NP_RStickY", "ControllerStickRightY"],
 		"name": "RS Y-",
 		"key": KEY_DOWN,
 		"range": Vector2(0, -1.0)
 	},
 	{
-		"input": "NP_RStickY",
+		"input": ["NP_RStickY", "ControllerStickRightY"],
 		"name": "RS Y+",
 		"key": KEY_UP
 	},
 ]
 
 func _ready():
+	# Nyarupad Inputs
 	Registry.add_parameter("NP_DPadDown", Vector2(0, 1), 0.0)
 	Registry.add_parameter("NP_DPadUp", Vector2(0, 1), 0.0)
 	Registry.add_parameter("NP_DPadLeft", Vector2(0, 1), 0.0)
@@ -201,6 +205,40 @@ func _ready():
 	Registry.add_parameter("NP_RThumbY", Vector2(-1.0, 1.0), 0.0)
 	Registry.add_parameter("NP_RStickX", Vector2(-1.0, 1.0), 0.0)
 	Registry.add_parameter("NP_RStickY", Vector2(-1.0, 1.0), 0.0)
+	
+	# VTS Gamepad Inputs
+	Registry.add_parameter("ControllerDPadX", Vector2(-1.0, 1.0), 0.0)
+	Registry.add_parameter("ControllerDPadY", Vector2(-1.0, 1.0), 0.0)
+	Registry.add_parameter("ControllerStickLeftX", Vector2(-1.0, 1.0), 0.0)
+	Registry.add_parameter("ControllerStickLeftY", Vector2(-1.0, 1.0), 0.0)
+	Registry.add_parameter("ControllerStickRightX", Vector2(-1.0, 1.0), 0.0)
+	Registry.add_parameter("ControllerStickRightY", Vector2(-1.0, 1.0), 0.0)
+	Registry.add_parameter("ControllerStickPressLeft", Vector2(0, 1), 0.0)
+	Registry.add_parameter("ControllerStickPressRight", Vector2(0, 1), 0.0)
+	Registry.add_parameter("ControllerTriangle", Vector2(0, 1), 0.0)
+	Registry.add_parameter("ControllerCross", Vector2(0, 1), 0.0)
+	Registry.add_parameter("ControllerSquare", Vector2(0, 1), 0.0)
+	Registry.add_parameter("ControllerCircle", Vector2(0, 1), 0.0)
+	Registry.add_parameter("ControllerShoulderLeft", Vector2(0, 1), 0.0)
+	Registry.add_parameter("ControllerTriggerLeft", Vector2(0, 1), 0.0)
+	Registry.add_parameter("ControllerShoulderRight", Vector2(0, 1), 0.0)
+	Registry.add_parameter("ControllerTriggerRight", Vector2(0, 1), 0.0)
+	Registry.add_parameter("ControllerOptionLeft", Vector2(0, 1), 0.0)
+	Registry.add_parameter("ControllerOptionRight", Vector2(0, 1), 0.0)
+	Registry.add_parameter("ControllerHome", Vector2(0, 1), 0.0)
+	Registry.add_parameter("ControllerTouchPadPress", Vector2(0, 1), 0.0)
+	Registry.add_parameter("ControllerButtonsAnyLeft", Vector2(0, 1), 0.0)
+	Registry.add_parameter("ControllerButtonsAnyRight", Vector2(0, 1), 0.0)
+	Registry.add_parameter("ControllerButtonCountLeft", Vector2(0, 99), 0.0)
+	Registry.add_parameter("ControllerButtonCountRight", Vector2(0, 99), 0.0)
+	Registry.add_parameter("ControllerLean", Vector2(-1, 1), 0.0)
+	Registry.add_parameter("ControllerThumbPosLeft", Vector2(0, 3), 0.0)
+	Registry.add_parameter("ControllerThumbPosRight", Vector2(0, 3), 0.0)
+	Registry.add_parameter("ControllerFingerPosLeft", Vector2(0, 2), 0.0)
+	Registry.add_parameter("ControllerFingerPosRight", Vector2(0, 2), 0.0)
+	Registry.add_parameter("ControllerOrientationX", Vector2(-60.0, 60.0), 0.0)
+	Registry.add_parameter("ControllerOrientationY", Vector2(-60.0, 60.0), 0.0)
+	Registry.add_parameter("ControllerOrientationZ", Vector2(-60.0, 60.0), 0.0)
 
 func create_config() -> Node:
 	var config = load("res://lib/tracking/gamepad/config.tscn").instantiate()
@@ -210,38 +248,91 @@ func create_config() -> Node:
 func read_gamepad() -> Dictionary:
 	return {
 		"NP_ButtonA": Input.is_joy_button_pressed(gamepad_index, JOY_BUTTON_A),
+		"ControllerCross": Input.is_joy_button_pressed(gamepad_index, JOY_BUTTON_A),
 		"NP_ButtonB": Input.is_joy_button_pressed(gamepad_index, JOY_BUTTON_B),
+		"ControllerCircle": Input.is_joy_button_pressed(gamepad_index, JOY_BUTTON_B),
 		"NP_ButtonX": Input.is_joy_button_pressed(gamepad_index, JOY_BUTTON_X),
+		"ControllerSquare": Input.is_joy_button_pressed(gamepad_index, JOY_BUTTON_X),
 		"NP_ButtonY": Input.is_joy_button_pressed(gamepad_index, JOY_BUTTON_Y),
+		"ControllerTriangle": Input.is_joy_button_pressed(gamepad_index, JOY_BUTTON_Y),
 		"NP_L1": Input.is_joy_button_pressed(gamepad_index, JOY_BUTTON_LEFT_SHOULDER),
+		"ControllerShoulderLeft": Input.is_joy_button_pressed(gamepad_index, JOY_BUTTON_LEFT_SHOULDER),
 		"NP_R1": Input.is_joy_button_pressed(gamepad_index, JOY_BUTTON_RIGHT_SHOULDER),
+		"ControllerShoulderRight": Input.is_joy_button_pressed(gamepad_index, JOY_BUTTON_RIGHT_SHOULDER),
 		"NP_L2": Input.get_joy_axis(gamepad_index, JOY_AXIS_TRIGGER_LEFT),
+		"ControllerTriggerLeft": Input.get_joy_axis(gamepad_index, JOY_AXIS_TRIGGER_LEFT),
 		"NP_R2": Input.get_joy_axis(gamepad_index, JOY_AXIS_TRIGGER_RIGHT),
+		"ControllerTriggerRight": Input.get_joy_axis(gamepad_index, JOY_AXIS_TRIGGER_RIGHT),
 		"NP_ButtonLS": Input.is_joy_button_pressed(gamepad_index, JOY_BUTTON_LEFT_STICK),
+		"ControllerStickPressLeft": Input.is_joy_button_pressed(gamepad_index, JOY_BUTTON_LEFT_STICK),
 		"NP_ButtonRS": Input.is_joy_button_pressed(gamepad_index, JOY_BUTTON_RIGHT_STICK),
+		"ControllerStickPressRight": Input.is_joy_button_pressed(gamepad_index, JOY_BUTTON_RIGHT_STICK),
 		"NP_SelectDown": Input.is_joy_button_pressed(gamepad_index, JOY_BUTTON_BACK),
+		"ControllerOptionLeft": Input.is_joy_button_pressed(gamepad_index, JOY_BUTTON_BACK),
 		"NP_StartDown": Input.is_joy_button_pressed(gamepad_index, JOY_BUTTON_START),
+		"ControllerOptionRight": Input.is_joy_button_pressed(gamepad_index, JOY_BUTTON_START),
 		"NP_LStickX": Input.get_joy_axis(gamepad_index, JOY_AXIS_LEFT_X),
+		"ControllerStickLeftX": Input.get_joy_axis(gamepad_index, JOY_AXIS_LEFT_X),
 		"NP_LStickY": Input.get_joy_axis(gamepad_index, JOY_AXIS_LEFT_Y),
+		"ControllerStickLeftY": Input.get_joy_axis(gamepad_index, JOY_AXIS_LEFT_Y),
 		"NP_RStickX": Input.get_joy_axis(gamepad_index, JOY_AXIS_RIGHT_X),
+		"ControllerStickRightX": Input.get_joy_axis(gamepad_index, JOY_AXIS_RIGHT_X),
 		"NP_RStickY": Input.get_joy_axis(gamepad_index, JOY_AXIS_RIGHT_Y),
+		"ControllerStickRightY": Input.get_joy_axis(gamepad_index, JOY_AXIS_RIGHT_Y),
+		# nyarupad has discrete dpad bindings
 		"NP_DPadDown": Input.is_joy_button_pressed(gamepad_index, JOY_BUTTON_DPAD_DOWN),
 		"NP_DPadUp": Input.is_joy_button_pressed(gamepad_index, JOY_BUTTON_DPAD_UP),
 		"NP_DPadLeft": Input.is_joy_button_pressed(gamepad_index, JOY_BUTTON_DPAD_LEFT),
 		"NP_DPadRight": Input.is_joy_button_pressed(gamepad_index, JOY_BUTTON_DPAD_RIGHT),
+		# TODO DS4 touchpad support
+		# TODO DS4 gyro (requires Godot >= 4.7)
 	}
 	
 func read_from_kb() -> Dictionary:
 	var input = {}
 	for mapping in keyboard_mapping:
-		var np_input = mapping.input
+		var input_bind = mapping.input
 		var key = mapping.key
 		var map_to_range = mapping.get("range", Vector2(0.0, 1.0))
-		input[np_input] = input.get(np_input, 0.0) + (
-			map_to_range.y if GlobalInput.is_key_pressed(key) else map_to_range.x
-		)
+		for i in input_bind:
+			input[i] = input.get(i, 0.0) + (
+				map_to_range.y if GlobalInput.is_key_pressed(key) else map_to_range.x
+			)
 	
 	return input
+	
+var previous_l_thumb_pos: int = 0
+var previous_r_thumb_pos: int = 0
+
+func get_thumb_pos(side: bool, dpad: Vector2, stick: Vector2, stick_button: float, option: float) -> float:
+	# on Option
+	var pos = previous_l_thumb_pos if not side else previous_r_thumb_pos
+	if option > 0.0:
+		pos = 2
+	# on stick
+	elif stick.length() > DEADZONE:
+		pos = 1
+	elif stick_button > 0.0:
+		pos = 1
+	# on dpad
+	elif dpad.length() > DEADZONE:
+		pos = 0
+	# TODO on touchpad (3)
+	
+	# record thumb state to blend between frames
+	if side:
+		previous_r_thumb_pos = pos
+	else:
+		previous_l_thumb_pos = pos
+	
+	return pos
+	
+func get_finger_pos(shoulder_button: float, trigger_button: float) -> float:
+	if trigger_button > 0:
+		if shoulder_button > 0:
+			return 2 # both
+		return 1 # trigger
+	return 0 # shoulder at rest
 	
 func _process(_delta: float) -> void:
 	var inputs: Dictionary
@@ -265,14 +356,38 @@ func _process(_delta: float) -> void:
 	]
 	var l_thumb = get_thumb_axis(dpad)
 	var r_thumb = get_thumb_axis(face)
+	var l_stick = Vector2(
+		inputs["NP_LStickX"],
+		inputs["NP_LStickY"]
+	)
+	var r_stick = Vector2(
+		inputs["NP_RStickX"],
+		inputs["NP_RStickY"]
+	)
 	var r_down = is_button_pressed(face)
 	var l_down = is_button_pressed(dpad)
-	
-	if dpad_to_ls:
-		inputs.merge({
-			"NP_LStickX": (0.7 if l_thumb.length() > 1.0 else 1.0) * sign(l_thumb.x),
-			"NP_LStickY": (0.7 if l_thumb.length() > 1.0 else 1.0) * sign(l_thumb.y)
-		}, true)
+	var l_thumb_pos = get_thumb_pos(
+		false,
+		l_thumb,
+		l_stick,
+		float(inputs["ControllerStickPressLeft"]),
+		float(inputs["ControllerOptionLeft"])
+	)
+	var r_thumb_pos = get_thumb_pos(
+		true,
+		r_thumb,
+		r_stick,
+		float(inputs["ControllerStickPressRight"]),
+		float(inputs["ControllerOptionRight"])
+	)
+	var l_finger_pos = get_finger_pos(
+		float(inputs["ControllerShoulderLeft"]),
+		float(inputs["ControllerTriggerLeft"])
+	)
+	var r_finger_pos = get_finger_pos(
+		float(inputs["ControllerShoulderRight"]),
+		float(inputs["ControllerTriggerRight"])
+	)
 	
 	inputs.merge({
 		"NP_LThumbX": l_thumb.x,
@@ -281,6 +396,24 @@ func _process(_delta: float) -> void:
 		"NP_RThumbY": r_thumb.y,
 		"NP_RButtonDown": float(r_down),
 		"NP_LButtonDown": float(l_down),
+		
+		"ControllerDPadX": l_thumb.x,
+		"ControllerDPadY": l_thumb.y,
+		"ControllerButtonsAnyRight": float(r_down),
+		"ControllerButtonsAnyLeft": float(l_down),
+		
+		"ControllerThumbPosLeft": l_thumb_pos,
+		"ControllerThumbPosRight": r_thumb_pos,
+		"ControllerFingerPosLeft": l_finger_pos,
+		"ControllerFingerPosRight": r_finger_pos,
 	})
 		
+	if dpad_to_ls:
+		inputs.merge({
+			"NP_LStickX": (0.7 if l_thumb.length() > 1.0 else 1.0) * sign(l_thumb.x),
+			"NP_LStickY": (0.7 if l_thumb.length() > 1.0 else 1.0) * sign(l_thumb.y),
+			"ControllerStickLeftX": (0.7 if l_thumb.length() > 1.0 else 1.0) * sign(l_thumb.x),
+			"ControllerStickLeftY": (0.7 if l_thumb.length() > 1.0 else 1.0) * sign(l_thumb.y)
+		}, true)
+	
 	update(inputs)
