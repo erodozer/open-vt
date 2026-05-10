fb9fa610158943dcf8d86451df6d4dc7e1d6a1a0
erodozer <ero@erodozer.moe>
2026-05-10 11:39:52 -0400

    build script improvements

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -3,17 +3,35 @@
 PROJ_ROOT=$(pwd)
 
 pyenv local 3.13.0
-pip install pipx scons
+pip install pipx scons --quiet
 	
 # build gd_cubism
 build_gdcubism () {
+	echo -e "\n#### Preparing gd_cubism\n\n"
+
 	WORKDIR=$PROJ_ROOT/thirdparty/gd_cubism
 	ADDONDIR=$PROJ_ROOT/addons/gd_cubism
 	NATIVELIB=$(find $WORKDIR/thirdparty -type f -iwholename "*/linux/x86_64/libLive2DCubismCore.so" | head -n 1)
 
+	if [ -z "$NATIVELIB" ]; then
+		cat <<- xx
+		CUBISM NATIVE LIBRARY: not found
+
+
+		Cubism SDK must be acquired and prepared ahead of time
+		Agreeing to terms of use are required for all developers building with the SDK
+	
+		https://www.live2d.com/en/sdk/download/native/
+
+
+		xx
+
+		return 1
+	fi
+
 	echo "CUBISM NATIVE LIBRARY:" $NATIVELIB
 
-	mkdir $ADDONDIR
+	mkdir -p $ADDONDIR
 	
 	cd $WORKDIR
 	
@@ -42,10 +60,12 @@ build_gdcubism () {
 }
 
 build_virtualcamera () {
+	echo -e "\n#### Preparing gd-virtualcamera\n\n"
+
 	WORKDIR=$PROJ_ROOT/thirdparty/gd-virtualcamera
 	ADDONDIR=$PROJ_ROOT/addons/gd-virtualcamera
 
-	mkdir $ADDONDIR
+	mkdir -p $ADDONDIR
 	
 	cd $WORKDIR
 	
@@ -62,10 +82,12 @@ build_virtualcamera () {
 }
 
 build_keylogger() {
+	echo -e "\n#### Preparing godot-keylogger\n\n"
+
 	WORKDIR=$PROJ_ROOT/thirdparty/godot-keylogger
 	ADDONDIR=$PROJ_ROOT/addons/keylogger
 
-	mkdir $ADDONDIR
+	mkdir -p $ADDONDIR
 
 	cd $WORKDIR
 
@@ -82,7 +104,7 @@ build_keylogger() {
 }
 
 # fetch submodules if they haven't been already
-git fetch --recurse-submodules
+git submodule update --init --recursive
 
 build_gdcubism
 build_virtualcamera
