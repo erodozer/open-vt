0c47cdeb7243dcfb8146e2543572c7645a615202
Hoshino Lina <lina@lina.yt>
2026-09-13 18:41:45 +0900

    macos: Use .moc3 file instead of .model3 for model selection
    The native macOS file picker (and macOS in general) does not support
    extensions containing dots. As a hacky workaround, use the .moc3 file
    for model selection on macOS (assuming the model3 file has the same
    basename).

--- lib/model/formats/l2d/model_loader.gd
+++ lib/model/formats/l2d/model_loader.gd
@@ -7,6 +7,9 @@ func model_format() -> StringName:
 	return "Live2D/Ayagami"
 	
 func supported_extension() -> String:
+	if OS.has_feature("macos"):
+		# macOS does not support extensions containing dots, use moc3 instead
+		return ".moc3"
 	return ".model3.json"
 
 func strategy() -> Script:
@@ -17,12 +20,20 @@ func load_data(path: String) -> ModelMeta:
 	
 	var base_dir = path.get_base_dir()
 	var base_name = path.get_file().trim_suffix(supported_extension())
+	var model3_file = path
+
+	if OS.has_feature("macos"):
+		if base_name.ends_with(".model3.json"):
+			base_name = base_name.trim_suffix(".model3.json")
+		if model3_file.ends_with(".moc3"):
+			model3_file = base_dir.path_join("%s.model3.json" % base_name)
+
 	var vt_file = base_dir.path_join("%s.vtube.json" % base_name)
 	var ovt_file = base_dir.path_join("%s.ovt.json" % base_name)
 
 	meta.name = base_name
 	meta.id = base_name
-	meta.model = path
+	meta.model = model3_file
 
 	if FileAccess.file_exists(vt_file):
 		var vtube_data = Files.read_json(vt_file)
@@ -30,7 +41,7 @@ func load_data(path: String) -> ModelMeta:
 		meta.name = vtube_data["Name"]
 		meta.id = vtube_data["ModelID"]
 	
-	var model_data = Files.read_json(path)
+	var model_data = Files.read_json(model3_file)
 	meta.path = path.get_base_dir()
 	meta.format = model_format()
 	meta.studio_parameters = vt_file
